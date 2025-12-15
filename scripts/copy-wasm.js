f7462a936f44e59dc47ba7541e7bd11a60e7b7d5
Артур Ефремов <aefremov.kamaz.digital>
2025-12-16 00:22:05 +0300

    add script

---
+++ scripts/copy-wasm.js
@@ -0,0 +1,23 @@
+import fs from 'fs';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+function copyFolderSync(src, dest) {
+    if (!fs.existsSync(dest)) fs.mkdirSync(dest, { recursive: true });
+    const entries = fs.readdirSync(src, { withFileTypes: true });
+    for (let entry of entries) {
+        const srcPath = path.join(src, entry.name);
+        const destPath = path.join(dest, entry.name);
+        if (entry.isDirectory()) {
+            copyFolderSync(srcPath, destPath);
+        } else {
+            fs.copyFileSync(srcPath, destPath);
+        }
+    }
+}
+
+copyFolderSync(path.join(__dirname, '../src/wasm-modules'), path.join(__dirname, '../dist/wasm-modules'));
+console.log('WASM modules copied to dist!');
